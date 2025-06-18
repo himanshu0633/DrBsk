c7689a0fc2de3b5282e68189e74e9b5bdd563228
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-06-18 17:40:57 +0530

    logout functionality

--- src/Pages/EditProfile/EditProfile.js
+++ src/Pages/EditProfile/EditProfile.js
@@ -92,6 +92,13 @@ const EditProfile = () => {
       toast.error("Error updating profile. Please try again.");
     }
   };
+  const handleLogout = (e) => {
+    e.preventDefault();
+    console.log('Logging out...');
+    sessionStorage.clear();
+    window.location.href = '/login';
+  };
+
 
   return (
     <>
@@ -142,7 +149,7 @@ const EditProfile = () => {
                 </svg>
                 Need Help?
               </a>
-              <a className="nav-item logout" href="#">
+              <a onClick={handleLogout} className="nav-item logout" href="#">
                 <svg className="nav-icon" viewBox="0 0 24 24">
                   <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" />
                 </svg>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,212 @@
+// import React, { useState, useEffect, useRef } from 'react';
+// import './Header.css';
+// import '../../App.css'
+// import logo from '../../logo/logo1.jpg';
+// import { useSelector } from 'react-redux';
+// import { ShoppingCart } from 'lucide-react';
+// import { Link } from 'react-router-dom';
+
+// const Header = () => {
+//   const cartItems = useSelector((state) => state.app.data);
+//   const cartCount = cartItems.length;
+//   const [showDropdown, setShowDropdown] = useState(false);
+//   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+//   const dropdownRef = useRef(null);
+//   const [showInput, setShowInput] = useState(false);
+//   const [pincode, setPincode] = useState('');
+//   const [locationName, setLocationName] = useState('');
+//   const [currentPincode, setCurrentPincode] = useState('');
+//   const logout = sessionStorage.clear()
+//   // const userData=sessionStorage.getItem('userData')
+
+//   const storedUser = sessionStorage.getItem('userData');
+//   const userData = storedUser ? JSON.parse(storedUser) : null;
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth < 768);
+//     };
+
+//     window.addEventListener('resize', handleResize);
+//     return () => window.removeEventListener('resize', handleResize);
+//   }, []);
+
+//   useEffect(() => {
+//     const handleClickOutside = (event) => {
+//       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+//         setShowDropdown(false);
+//       }
+//     };
+
+//     if (showDropdown) {
+//       document.addEventListener('mousedown', handleClickOutside);
+//     }
+
+//     return () => {
+//       document.removeEventListener('mousedown', handleClickOutside);
+//     };
+//   }, [showDropdown]);
+
+//   const toggleDropdown = (e) => {
+//     e.stopPropagation();
+//     console.log("Current state:", showDropdown);
+//     setShowDropdown(!showDropdown);
+//   };
+
+//   const handleLogout = (e) => {
+//     e.preventDefault();
+//     console.log('Logging out...');
+//     sessionStorage.clear()
+//     window.location.href = '/login';
+//   };
+
+//   const handleDivClick = () => {
+//     setShowInput(true);
+//   };
+
+//   const handleInputChange = (e) => {
+//     setPincode(e.target.value);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const res = await fetch(`https://nominatim.openstreetmap.org/search?postalcode=${pincode}&country=India&format=json`);
+//       const data = await res.json();
+
+//       if (data.length > 0) {
+//         const location = data[0].display_name;
+//         console.log("dkhfkwhdrsjjk", location);
+
+//         setLocationName(`${location}`);
+//         setCurrentPincode(pincode);
+//       } else {
+//         setLocationName("Location not found");
+//         setCurrentPincode(pincode);
+//       }
+//     } catch (error) {
+//       console.error("Error fetching location:", error);
+//       setLocationName("Error fetching location");
+//       setCurrentPincode(pincode);
+//     }
+
+//     setShowInput(false);
+//     setPincode('');
+//   };
+
+//   return (
+//     <div >
+//       <header className="header-container">
+//         <div className=" ">
+//           <div className='header1'>
+//             <a className='logo_size' href="/">
+//               <img src={logo} alt="Logo" className="logo" />
+//             </a>
+//             <div className='headerLocationHideLgScreen flexProp' >
+//               <div className='position_relative'>
+//                 <div className="location-box order_4 " onClick={handleDivClick}>
+//                   <span className="location-icon">📍</span>
+//                   <span className="pincode">{currentPincode}</span>
+//                   <span className="location-name">{locationName}</span>
+//                   <span className="dropdown-icon">▼</span>
+//                 </div>
+//                 {showInput && (
+//                   <form onSubmit={handleSubmit} className='pincodeUi' >
+
+//                     <h2 className='chooseLocationHead'>Choose Location</h2>
+//                     <div className='borderlocation'></div>
+//                     <h2 className='pincodeLabel'>Enter Pincode:</h2>
+//                     <input
+//                       type="text"
+//                       placeholder="Enter pincode"
+//                       value={pincode}
+//                       onChange={handleInputChange}
+//                       className='pincodeInput'
+//                     />
+//                     <button type="submit" className='submitBtnForm'>Submit</button>
+//                   </form>
+//                 )}
+//               </div>
+
+//               {userData?.type === "wholesalePartner" ? null : <a href="/Prescription" className="no-decoration order_5">
+//                 <div className="upload-box">
+//                   <span className="upload-icon">📄</span>
+//                   <span className="location-name">Upload Prescription</span>
+//                 </div>
+//               </a>}
+
+
+//             </div>
+//             <div className='navbarIconFlex '>
+//               {/* <a href="/Cart" className="no-decoration">
+//                 <div className="cart-icon">🛒 Cart</div>
+//                  {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+//               </a> */}
+
+//               <Link to="/cart" className="cart-link">
+//                 <ShoppingCart size={30} />
+//                 {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+//               </Link>
+
+//               {/* <a href="/Notifications" className="no-decoration">
+//                 <div className="bell-icon">🔔</div>
+//               </a> */}
+//               <div className="signin-dropdown-wrapper" ref={dropdownRef}>
+//                 <div className="signin-btn" onClick={toggleDropdown}>
+//                   <div className="signin-icon">👤</div>
+//                   {userData ? null : <span className='margin_left_8'>Sign In</span>}
+//                 </div>
+//                 {showDropdown && (
+//                   <div className="dropdown-menu">
+//                     <a className="dropdown-item" href="/EditProfile">
+//                       <span className="dropdown-icon">👤</span> My Profile
+//                     </a>
+//                     <a className="dropdown-item" href="/OrderPage">
+//                       <span className="dropdown-icon">📦</span> My Orders
+//                     </a>
+//                     {/* <a className="dropdown-item" href="/patients">
+//                       <span className="dropdown-icon">👩‍⚕️</span> Patients
+//                     </a>
+//                     <a className="dropdown-item" href="/notifications">
+//                       <span className="dropdown-icon">🔔</span> Notifications
+//                     </a>
+//                     <a className="dropdown-item" href="/consultations">
+//                       <span className="dropdown-icon">💬</span> Consultations
+//                     </a> */}
+//                     <a href="#" className="dropdown-item" onClick={handleLogout}>
+//                       <span className="dropdown-icon">🚪</span> Logout
+//                     </a>
+//                   </div>
+//                 )}
+//               </div>
+//             </div>
+//           </div>
+//           <div className='headerLocationShowSmlScreen flexProp'>
+//             <div className="location-box order_4 ">
+//               <span className="location-icon">📍</span>
+//               <span className="pincode">{currentPincode}</span>
+//               <span className="location-name">{locationName}</span>
+//               <span className="dropdown-icon">▼</span>
+//             </div>
+//             <a href="/Prescription" className="no-decoration order_5">
+//               <div className="upload-box">
+//                 <span className="upload-icon">📄</span>
+//                 <span className="location-name">Upload Prescription</span>
+//               </div>
+//             </a>
+//           </div>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
 import React, { useState, useEffect, useRef } from 'react';
 import './Header.css';
-import '../../App.css'
+import '../../App.css';
 import logo from '../../logo/logo1.jpg';
 import { useSelector } from 'react-redux';
 import { ShoppingCart } from 'lucide-react';
@@ -17,6 +223,11 @@ const Header = () => {
   const [locationName, setLocationName] = useState('');
   const [currentPincode, setCurrentPincode] = useState('');
 
+  // Get userData from sessionStorage
+  const storedUser = sessionStorage.getItem('userData');
+  const userData = storedUser ? JSON.parse(storedUser) : null;
+
+  // Listen for screen resize events
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
@@ -51,6 +262,7 @@ const Header = () => {
   const handleLogout = (e) => {
     e.preventDefault();
     console.log('Logging out...');
+    sessionStorage.clear();
     window.location.href = '/login';
   };
 
@@ -71,8 +283,6 @@ const Header = () => {
 
       if (data.length > 0) {
         const location = data[0].display_name;
-        console.log("dkhfkwhdrsjjk", location);
-
         setLocationName(`${location}`);
         setCurrentPincode(pincode);
       } else {
@@ -88,28 +298,25 @@ const Header = () => {
     setShowInput(false);
     setPincode('');
   };
-  const storedUser = sessionStorage.getItem('userData');
-  const userData = storedUser ? JSON.parse(storedUser) : null;
 
   return (
-    <div >
+    <div>
       <header className="header-container">
-        <div className=" ">
+        <div className="">
           <div className='header1'>
             <a className='logo_size' href="/">
               <img src={logo} alt="Logo" className="logo" />
             </a>
-            <div className='headerLocationHideLgScreen flexProp' >
+            <div className='headerLocationHideLgScreen flexProp'>
               <div className='position_relative'>
-                <div className="location-box order_4 " onClick={handleDivClick}>
+                <div className="location-box order_4" onClick={handleDivClick}>
                   <span className="location-icon">📍</span>
                   <span className="pincode">{currentPincode}</span>
                   <span className="location-name">{locationName}</span>
                   <span className="dropdown-icon">▼</span>
                 </div>
                 {showInput && (
-                  <form onSubmit={handleSubmit} className='pincodeUi' >
-
+                  <form onSubmit={handleSubmit} className='pincodeUi'>
                     <h2 className='chooseLocationHead'>Choose Location</h2>
                     <div className='borderlocation'></div>
                     <h2 className='pincodeLabel'>Enter Pincode:</h2>
@@ -131,29 +338,20 @@ const Header = () => {
                   <span className="location-name">Upload Prescription</span>
                 </div>
               </a>}
-
-
             </div>
-            <div className='navbarIconFlex '>
-              {/* <a href="/Cart" className="no-decoration">
-                <div className="cart-icon">🛒 Cart</div>
-                 {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
-              </a> */}
-
+            <div className='navbarIconFlex'>
               <Link to="/cart" className="cart-link">
                 <ShoppingCart size={30} />
                 {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
               </Link>
 
-              {/* <a href="/Notifications" className="no-decoration">
-                <div className="bell-icon">🔔</div>
-              </a> */}
               <div className="signin-dropdown-wrapper" ref={dropdownRef}>
-                <div className="signin-btn" onClick={toggleDropdown}>
+
+                {userData ? <div className="signin-btn" onClick={toggleDropdown}>
                   <div className="signin-icon">👤</div>
-                  {userData ? null : <span className='margin_left_8'>Sign In</span>}
-                </div>
-                {showDropdown && (
+                  <span className='margin_left_8'>Sign In</span>
+                </div> : null}
+                {showDropdown && userData && (
                   <div className="dropdown-menu">
                     <a className="dropdown-item" href="/EditProfile">
                       <span className="dropdown-icon">👤</span> My Profile
@@ -161,15 +359,6 @@ const Header = () => {
                     <a className="dropdown-item" href="/OrderPage">
                       <span className="dropdown-icon">📦</span> My Orders
                     </a>
-                    {/* <a className="dropdown-item" href="/patients">
-                      <span className="dropdown-icon">👩‍⚕️</span> Patients
-                    </a>
-                    <a className="dropdown-item" href="/notifications">
-                      <span className="dropdown-icon">🔔</span> Notifications
-                    </a>
-                    <a className="dropdown-item" href="/consultations">
-                      <span className="dropdown-icon">💬</span> Consultations
-                    </a> */}
                     <a href="#" className="dropdown-item" onClick={handleLogout}>
                       <span className="dropdown-icon">🚪</span> Logout
                     </a>
@@ -179,7 +368,7 @@ const Header = () => {
             </div>
           </div>
           <div className='headerLocationShowSmlScreen flexProp'>
-            <div className="location-box order_4 ">
+            <div className="location-box order_4">
               <span className="location-icon">📍</span>
               <span className="pincode">{currentPincode}</span>
               <span className="location-name">{locationName}</span>
@@ -199,6 +388,5 @@ const Header = () => {
 };
 
 export default Header;
-
 
 
