813b8ffd7b7c1c84be96d26ac32c72ed6cfe7494
Nikhil Kumar <nik326990@gmail.com>
2025-06-18 11:59:34 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -69,11 +69,11 @@ const Header = () => {
       const res = await fetch(`https://nominatim.openstreetmap.org/search?postalcode=${pincode}&country=India&format=json`);
       const data = await res.json();
 
-      if (data.length > 0) {
-        const location = data[0].display_name;
-        console.log("dkhfkwhdrsjjk", location);
+      if (data?.length > 0) {
+        const location = data[0]?.display_name;
+        // console.log("dkhfkwhdrsjjk", location);
         const placeName =
-          location.city || location.town || location.village || location.state || "Unknown location";
+          location?.city || location?.town || location?.village || location?.state || "Unknown location";
 
         setLocationName(`${location}`);
         setCurrentPincode(pincode);
