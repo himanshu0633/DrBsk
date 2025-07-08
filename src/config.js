845e675fe13243eafb288cc06d3da1da843424c0
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-07-08 18:20:16 +0530

    removed s

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'https://31.97.61.81:4000'
+const API_URL = 'http://31.97.61.81:4000'
 
 // const API_URL = window.location.protocol === 'https:'
 //     ? 'https://drbskhealthcare.com'
