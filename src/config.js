2211a54fdd77b29bbc6d4a3c9b3d33362c0ca8f1
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-07-08 10:54:05 +0530

    config ip url

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 
-const API_URL = window.location.protocol === 'https:'
-    ? 'https://drbskhealthcare.com'
-    : window.location.hostname === 'drbskhealthcare.in'
-        ? 'http://drbskhealthcare.in:4000'
-        : 'http://31.97.61.81:4000';
+// const API_URL = window.location.protocol === 'https:'
+//     ? 'https://drbskhealthcare.com'
+//     : window.location.hostname === 'drbskhealthcare.in'
+//         ? 'http://drbskhealthcare.in:4000'
+//         : 'http://31.97.61.81:4000';
 
-// const API_URL = 'http://31.97.61.81:4000'
+const API_URL = 'http://31.97.61.81:4000'
 // const API_URL =  'http://localhost:5000'
 // const API_URL = 'https://mtgxcv03-5000.inc1.devtunnels.ms'
 
