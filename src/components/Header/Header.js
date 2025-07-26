4180e0c49fc59d081466f4118d1bb2c8ee0651c9
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-07-26 11:59:28 +0530

    location saved

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,235 @@
+// import React, { useState, useEffect, useRef } from 'react';
+// import './Header.css';
+// import '../../App.css';
+// import logo from '../../logo/logo1.jpg';
+// import { useSelector } from 'react-redux';
+// import { ShoppingCart } from 'lucide-react';
+// import { Link, useNavigate } from 'react-router-dom';
+
+// const Header = () => {
+//   const cartItems = useSelector((state) => state.app.data);
+//   const cartCount = cartItems.length;
+//   const [showDropdown, setShowDropdown] = useState(false);
+//   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+//   const dropdownRef = useRef(null);
+//   const [showInput, setShowInput] = useState(false);
+//   const [pincode, setPincode] = useState('');
+//   const [locationName, setLocationName] = useState('');
+//   const [currentPincode, setCurrentPincode] = useState('');
+
+//   // Get userData from sessionStorage
+//   const storedUser = sessionStorage.getItem('userData');
+//   const userData = storedUser ? JSON.parse(storedUser) : null;
+//   const navigate = useNavigate();
+
+//   // Listen for screen resize events
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth < 768);
+//     };
+
+//     window.addEventListener('resize', handleResize);
+//     return () => window.removeEventListener('resize', handleResize);
+//   }, []);
+
+
+//   useEffect(() => {
+//     const handleClickOutside = (event) => {
+//       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+//         setShowDropdown(false);
+//       }
+//     };
+
+//     if (showDropdown) {
+//       document.addEventListener('mousedown', handleClickOutside);
+//     } else {
+//       document.removeEventListener('mousedown', handleClickOutside);
+//     }
+
+//     return () => {
+//       document.removeEventListener('mousedown', handleClickOutside);
+//     };
+//   }, [showDropdown]);
+
+//   const toggleDropdown = (e) => {
+//     e.stopPropagation();
+//     console.log("Current state:", showDropdown);
+//     setShowDropdown(!showDropdown);
+//   };
+
+//   const handleLogout = (e) => {
+//     e.preventDefault();
+//     console.log('Logging out...');
+//     // sessionStorage.clear();
+//     // window.location.href = '/login';
+//     sessionStorage.removeItem('userData');
+//     setShowDropdown(false);
+//     navigate('/login')
+//   };
+
+//   const handleDivClick = () => {
+//     setShowInput(true);
+//   };
+
+//   const handleInputChange = (e) => {
+//     setPincode(e.target.value);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const res = await fetch(`https://nominatim.openstreetmap.org/search?postalcode=${pincode}&country=India&format=json`);
+//       const data = await res.json();
+
+//       if (data.length > 0) {
+//         const location = data[0].display_name;
+//         setLocationName(`${location}`);
+//         setCurrentPincode(pincode);
+//       } else {
+//         setLocationName("Location not found");
+//         setCurrentPincode(pincode);
+//       }
+//     } catch (error) {
+//       console.error("Error fetching location:", error);
+//       setLocationName("Error fetching location");
+//       setCurrentPincode(pincode);
+//     }
+
+//     setShowInput(false);
+//     setPincode('');
+//   };
+
+//   return (
+//     <div>
+//       <header className="header-container">
+//         <div className="">
+//           <div className='header1'>
+//             <a className='logo_size' href="/">
+//               <img src={logo} alt="Logo" className="logo" />
+//             </a>
+//             <div className='headerLocationHideLgScreen flexProp'>
+//               <div className='position_relative'>
+//                 <div className="location-box order_4" onClick={handleDivClick}>
+//                   <span className="location-icon">📍</span>
+//                   <span className="pincode">{currentPincode}</span>
+//                   <span className="location-name">{locationName}</span>
+//                   <span className="dropdown-icon">▼</span>
+//                 </div>
+//                 {showInput && (
+//                   <form onSubmit={handleSubmit} className='pincodeUi'>
+//                     <h2 className='chooseLocationHead'>Choose Location</h2>
+//                     <div className='borderlocation'></div>
+//                     <h2 className='pincodeLabel'>Enter Pincode:</h2>
+//                     <input
+//                       type="text"
+//                       placeholder="Enter pincode"
+//                       value={pincode}
+//                       onChange={handleInputChange}
+//                       className='pincodeInput'
+//                     />
+//                     <button type="submit" className='submitBtnForm'>Submit</button>
+//                   </form>
+//                 )}
+//               </div>
+
+//               {userData?.type === "wholesalePartner" ? null : <a onClick={() => navigate('/Prescription')} className="no-decoration order_5">
+//                 <div className="upload-box">
+//                   <span className="upload-icon">📄</span>
+//                   <span className="location-name">Upload Prescription</span>
+//                 </div>
+//               </a>}
+//             </div>
+//             <div className='navbarIconFlex'>
+//               <a onClick={() => navigate('/cart')} className="cart-link">
+//                 <ShoppingCart size={30} />
+//                 {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+//               </a>
+
+//               {/* <div className="signin-dropdown-wrapper" ref={dropdownRef}>
+//                 <div className="signin-btn" onClick={toggleDropdown}>
+//                   <div className="signin-icon">👤</div>
+//                   <span className='margin_left_8'>Sign In</span>
+//                 </div>
+//                 {showDropdown && userData && (
+//                   <div className="dropdown-menu">
+//                     <a className="dropdown-item" href="/EditProfile">
+//                       <span className="dropdown-icon">👤</span> My Profile
+//                     </a>
+//                     <a className="dropdown-item" href="/OrderPage">
+//                       <span className="dropdown-icon">📦</span> My Orders
+//                     </a>
+//                     <a href="#" className="dropdown-item" onClick={handleLogout}>
+//                       <span className="dropdown-icon">🚪</span> Logout
+//                     </a>
+//                   </div>
+//                 )}
+//               </div> */}
+
+
+//               <div className="signin-dropdown-wrapper" ref={dropdownRef}>
+//                 {/* Show sign-in button only when userData is not available */}
+//                 {!userData ? (
+//                   <div className="signin-btn" onClick={toggleDropdown}>
+//                     <div className="signin-icon">👤</div>
+//                     <span className='margin_left_8'>Sign In</span>
+//                   </div>
+//                 ) : (
+//                   <div className="signin-btn" onClick={toggleDropdown}>
+//                     <div className="signin-icon">👤</div>
+//                     <span className='margin_left_8'>Profile</span>
+//                   </div>
+//                 )}
+
+//                 {/* Show the dropdown menu only if userData exists */}
+//                 {showDropdown && userData && (
+//                   <div className="dropdown-menu">
+//                     <a className="dropdown-item" onClick={()=>{navigate('/EditProfile')}}>
+//                       <span className="dropdown-icon">👤</span> My Profile
+//                     </a>
+//                     <a className="dropdown-item" onClick={()=>{navigate('/OrderPage')}} >
+//                       <span className="dropdown-icon">📦</span> My Orders
+//                     </a>
+//                     <a href="#" className="dropdown-item" onClick={handleLogout}>
+//                       <span className="dropdown-icon">🚪</span> Logout
+//                     </a>
+//                   </div>
+//                 )}
+//                 {/* If not logged in, redirect to login page */}
+//                 {!userData && showDropdown && (
+//                   <div className="dropdown-menu">
+//                     <a className="dropdown-item" onClick={() => navigate('/login')}>
+//                       <span className="dropdown-icon">🔑</span> Go to Login
+//                     </a>
+//                   </div>
+//                 )}
+//               </div>
+
+//             </div>
+//           </div>
+//           <div className='headerLocationShowSmlScreen flexProp'>
+//             <div className="location-box order_4">
+//               <span className="location-icon">📍</span>
+//               <span className="pincode">{currentPincode}</span>
+//               <span className="location-name">{locationName}</span>
+//               <span className="dropdown-icon">▼</span>
+//             </div>
+//             <a href="/Prescription" className="no-decoration order_5">
+//               <div className="upload-box">
+//                 <span className="upload-icon">📄</span>
+//                 <span className="location-name">Upload Prescription</span>
+//               </div>
+//             </a>
+//           </div>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
+
 import React, { useState, useEffect, useRef } from 'react';
 import './Header.css';
 import '../../App.css';
@@ -22,6 +254,16 @@ const Header = () => {
   const userData = storedUser ? JSON.parse(storedUser) : null;
   const navigate = useNavigate();
 
+  // Retrieve the last pincode and location from sessionStorage if available
+  useEffect(() => {
+    const storedPincode = sessionStorage.getItem('lastPincode');
+    const storedLocation = sessionStorage.getItem('lastLocation');
+    if (storedPincode && storedLocation) {
+      setCurrentPincode(storedPincode);
+      setLocationName(storedLocation);
+    }
+  }, []);
+
   // Listen for screen resize events
   useEffect(() => {
     const handleResize = () => {
@@ -31,7 +273,6 @@ const Header = () => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -60,11 +301,9 @@ const Header = () => {
   const handleLogout = (e) => {
     e.preventDefault();
     console.log('Logging out...');
-    // sessionStorage.clear();
-    // window.location.href = '/login';
     sessionStorage.removeItem('userData');
     setShowDropdown(false);
-    navigate('/login')
+    navigate('/login');
   };
 
   const handleDivClick = () => {
@@ -84,16 +323,28 @@ const Header = () => {
 
       if (data.length > 0) {
         const location = data[0].display_name;
-        setLocationName(`${location}`);
+        setLocationName(location);
         setCurrentPincode(pincode);
+
+        // Save the pincode and location to sessionStorage
+        sessionStorage.setItem('lastPincode', pincode);
+        sessionStorage.setItem('lastLocation', location);
       } else {
         setLocationName("Location not found");
         setCurrentPincode(pincode);
+
+        // Save the invalid pincode and location to sessionStorage
+        sessionStorage.setItem('lastPincode', pincode);
+        sessionStorage.setItem('lastLocation', "Location not found");
       }
     } catch (error) {
       console.error("Error fetching location:", error);
       setLocationName("Error fetching location");
       setCurrentPincode(pincode);
+
+      // Save the invalid pincode and location to sessionStorage
+      sessionStorage.setItem('lastPincode', pincode);
+      sessionStorage.setItem('lastLocation', "Error fetching location");
     }
 
     setShowInput(false);
@@ -146,27 +397,6 @@ const Header = () => {
                 {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
               </a>
 
-              {/* <div className="signin-dropdown-wrapper" ref={dropdownRef}>
-                <div className="signin-btn" onClick={toggleDropdown}>
-                  <div className="signin-icon">👤</div>
-                  <span className='margin_left_8'>Sign In</span>
-                </div>
-                {showDropdown && userData && (
-                  <div className="dropdown-menu">
-                    <a className="dropdown-item" href="/EditProfile">
-                      <span className="dropdown-icon">👤</span> My Profile
-                    </a>
-                    <a className="dropdown-item" href="/OrderPage">
-                      <span className="dropdown-icon">📦</span> My Orders
-                    </a>
-                    <a href="#" className="dropdown-item" onClick={handleLogout}>
-                      <span className="dropdown-icon">🚪</span> Logout
-                    </a>
-                  </div>
-                )}
-              </div> */}
-
-
               <div className="signin-dropdown-wrapper" ref={dropdownRef}>
                 {/* Show sign-in button only when userData is not available */}
                 {!userData ? (
@@ -184,10 +414,10 @@ const Header = () => {
                 {/* Show the dropdown menu only if userData exists */}
                 {showDropdown && userData && (
                   <div className="dropdown-menu">
-                    <a className="dropdown-item" onClick={()=>{navigate('/EditProfile')}}>
+                    <a className="dropdown-item" onClick={() => { navigate('/EditProfile') }}>
                       <span className="dropdown-icon">👤</span> My Profile
                     </a>
-                    <a className="dropdown-item" onClick={()=>{navigate('/OrderPage')}} >
+                    <a className="dropdown-item" onClick={() => { navigate('/OrderPage') }}>
                       <span className="dropdown-icon">📦</span> My Orders
                     </a>
                     <a href="#" className="dropdown-item" onClick={handleLogout}>
@@ -204,7 +434,6 @@ const Header = () => {
                   </div>
                 )}
               </div>
-
             </div>
           </div>
           <div className='headerLocationShowSmlScreen flexProp'>
@@ -228,5 +457,6 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
