6d3710c21ff9b101938fcb678e0aef8953c56cec
Ankita-Sehrawat <sehrawatankita1@gmail.com>
2025-09-16 19:00:17 +0530

    removed commented apiurl in config

--- src/config.js
+++ src/config.js
@@ -1,18 +1,8 @@
-
 // const API_URL = 'http://31.97.61.81:4000'
 
 const API_URL = 'https://drbskhealthcare.com/';
 
 // const API_URL = "https://fvvcbrpm-4000.inc1.devtunnels.ms/"
 
-// const API_URL = 'http://localhost:4000'
-
-// const API_URL = window.location.protocol === 'https:'
-//     ? 'https://drbskhealthcare.com'
-//     : window.location.hostname === 'drbskhealthcare.in'
-//         ? 'http://drbskhealthcare.in:4000'
-//         : 'http://31.97.61.81:4000';
-// const API_URL =  'http://localhost:5000'
-// const API_URL = 'https://fvvcbrpm-4000.inc1.devtunnels.ms/'
 
 export default API_URL
