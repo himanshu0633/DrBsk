53c285f788e9b62a2c100fe530c6f24c97574256
deepanshu <deep.nalhera476@gmail.com>
2026-02-02 12:42:26 +0530

    fghj

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
-const API_URL = 'http://localhost:4000'
+// const API_URL = 'http://localhost:4000'
 
-// const API_URL = 'https://drbskhealthcare.com/';
+const API_URL = 'https://drbskhealthcare.com/';
 
 // const API_URL = "https://j449fxt7-4000.inc1.devtunnels.ms/"
+
 
 export default API_URL
 
